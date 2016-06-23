82e0a7f35a1f3dece8e5fbd3a3c29b4c5e88842b
Vitaly Dudarev <vitalydudarev@gmail.com>
2016-06-23 22:39:31 +0300

    Code cleanup.

--- worker.py
+++ worker.py
@@ -4,22 +4,22 @@ import ioutils
 import config
 import localimage
 
-class WorkerProxy:
-    started = False
 
+class WorkerProxy:
     def __init__(self):
-        self.worker = Worker()
-        self.worker.init()
+        self.__worker = Worker()
+        self.__worker.init()
+        self.__started = False
 
     def get_result(self, get_image_link, get_thumb_link):
-        if not self.started:
-            t = threading.Thread(target=self.worker.work)
+        if not self.__started:
+            t = threading.Thread(target=self.__worker.work)
             t.start()
-            self.started = True
-        
-        images = self.worker.get_result()
-        total = self.worker.get_total_count()
-        processed = self.worker.get_processed_count()
+            self.__started = True
+
+        images = self.__worker.get_result()
+        total = self.__worker.get_total_count()
+        processed = self.__worker.get_processed_count()
 
         imgs = {}
 
@@ -28,65 +28,66 @@ class WorkerProxy:
             img = get_image_link(image)
             imgs[img] = thumb
 
-        return {'images':imgs, 'total':total, 'processed':processed}
+        return {'images': imgs, 'total': total, 'processed': processed}
+
 
 class Worker:
     THREAD_COUNT = 2
 
-    q = Queue.Queue()
-    result = []
-    total_count = 0
-    processed_count = 0
-    files_to_process = []
+    def __init__(self):
+        self.__queue = Queue.Queue()
+        self.__result = []
+        self.__total_count = 0
+        self.__processed_count = 0
+        self.__files_to_process = 0
 
     def get_processed_count(self):
-        return self.processed_count
+        return self.__processed_count
 
     def get_total_count(self):
-        return self.total_count
+        return self.__total_count
 
     def get_result(self):
-        res = self.result
+        res = self.__result
         partial_res = []
         for item in res:
             partial_res.append(item)
 
-        self.processed_count += len(self.result)
-        del self.result[:]
+        self.__processed_count += len(self.__result)
+        del self.__result[:]
         return partial_res
 
     def get_current_count(self):
-        return len(self.result)
+        return len(self.__result)
 
     def do_work(self):
         while True:
-            file_name = self.q.get()
+            file_name = self.__queue.get()
             thumb_file_name = ioutils.get_thumb_name(file_name, config.THUMB_PATH)
             localimage.thumb(file_name, thumb_file_name, config.THUMB_SIZE)
-            self.result.append(file_name)
-            self.q.task_done()
+            self.__result.append(file_name)
+            self.__queue.task_done()
 
     def init(self):
         ioutils.create_directory(config.THUMB_PATH)
         files = ioutils.get_files(config.IMAGES_PATH, config.EXTENSIONS)
 
         images_count = len(files)
-        if (images_count > config.LIMIT):
+        if images_count > config.LIMIT:
             images_count = config.LIMIT
 
-        self.files_to_process = files
-        self.total_count = images_count
+        self.__files_to_process = files
+        self.__total_count = images_count
 
     def work(self):
-        if (self.total_count > 0):
-            res = {}
+        if self.__total_count > 0:
 
-            for i in range(0, self.total_count):
-                self.q.put(self.files_to_process[i])
+            for i in range(0, self.__total_count):
+                self.__queue.put(self.__files_to_process[i])
 
             for i in range(0, self.THREAD_COUNT):
                 t = threading.Thread(target=self.do_work)
                 t.daemon = True
                 t.start()
 
-            self.q.join()
+            self.__queue.join()
